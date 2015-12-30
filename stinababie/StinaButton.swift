d8c8a8ff89e5567d5404b5df6b2e49105e9a9483
Vu Dang <jublusion5@gmail.com>
2015-12-29 22:45:59 -0600

    Added labels into Stackview for front page

---
+++ stinababie/StinaButton.swift
@@ -0,0 +1,21 @@
+//
+//  StinaButton.swift
+//  stinababie
+//
+//  Created by Vu Dang on 12/29/15.
+//  Copyright © 2015 Vu Dang. All rights reserved.
+//
+
+import UIKit
+
+class StinaButton: UIButton {
+
+    /*
+    // Only override drawRect: if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func drawRect(rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
